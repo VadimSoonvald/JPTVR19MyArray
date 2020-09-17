f6df2ff758b7dc13f613d2cbe05ea10051d837b6
pupil <pupil@ivkhk.ee>
2020-09-17 09:00:04 +0300

    Готовая работа с массивами

---
+++ src/jptvr19myarray/App.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19myarray;
+
+import java.util.Random;
+
+/**
+ *
+ * @author pupil
+ */
+class App {
+    void run(){
+        int sum = 0; 
+        System.out.println("***** Программа MyArray ****");
+        System.out.println("Создать массив int c названием MyArray с 20 ячейками");
+        int myArray[] = new int[20];
+        System.out.println("Заполнить массив случайными значениями в диапозоне от 0 до 49");
+        Random random = new Random();
+        for(int i = 0; i < myArray.length; i++){
+            myArray[i] = random.nextInt (49-0+1)-0;
+        }
+        System.out.println("Вывести значения массива на экран");
+        for(int i = 0; i < myArray.length; i++){
+            System.out.printf("%3d",myArray[i]);
+        }System.out.println();
+        System.out.println("Вывести значения массива на экран в обратном порядке");
+        for(int i = myArray.length-1; i>=0; i--){
+            System.out.printf("%3d",myArray[i]);
+        } System.out.println();
+        System.out.println("Суммая ячеек: ");
+        for(int i = 0; i < myArray.length; i++){
+            sum = sum + myArray[i];
+        }System.out.println(sum);
+        
+        System.out.println("Первая половина массива: ");
+        for(int i = 0; i < myArray.length / 2; i++){
+            System.out.printf("%3d",myArray[i]);
+        }System.out.println();
+        
+        System.out.println("Вторая половина массива: ");
+        for(int i = myArray.length / 2; i < myArray.length; i++){
+            System.out.printf("%3d",myArray[i]);
+        }System.out.println();
+        }
+    }
